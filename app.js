84ab7acdaedc6a80d8564e1f37a6a3c8260e5abf
abrar-hossain <abrarmashuk@gmail.com>
2021-09-02 17:56:24 +0600

    something

--- app.js
+++ app.js
@@ -35,13 +35,9 @@ const searchBook = async () => {
     const data = await res.json();
 
     displayBooks(data.docs, data.numFound);
-    //displayNumber(data.numFound);
+
 }
-//display total book
-/* const displayNumber = total => {
-    document.getElementById('found').innerHTML = `${total} books found`;
-    toggleSpinner('none');
-} */
+
 //display books from api
 const displayBooks = (books, numbers) => {
     //console.log(books);
@@ -51,23 +47,24 @@ const displayBooks = (books, numbers) => {
         toggleSpinner('none');
         return;
     }
-    document.getElementById('found').innerHTML = `${numbers} books found`;
-    //check undefined
+
+    //check undefined(set no value) data
     const filterBooks = books.filter(info => info.cover_i !== undefined && info.author_name !== undefined && info.first_publish_year !== undefined && info.publisher !== undefined);
+    document.getElementById('errorMessage').innerText = `${numbers} books found`;
     //array loop
-    const container = document.getElementById('search-result');
     filterBooks.slice(0, 30).forEach(filterBook => {
         console.log(filterBook);
+        const container = document.getElementById('search-result');
         const div = document.createElement('div');
         div.classList.add('col');
+
         div.innerHTML = `
-    
             <div class="card h-100">
-                <div class="card-body border border-success">
+                 <div class="card-body border border-success">
                     <img class="card-img-top mb-5" src="https://covers.openlibrary.org/b/id/${filterBook.cover_i}-M.jpg">
                         <h1 class="card-title text-center"> ${filterBook.title}</h1>
-                        <h3 class="card-text"> author: ${filterBook.author_name} </h3>
-                        <h4 class="card-text"> publisher: ${filterBook.publisher} </h4>
+                        <h3 class="card-text"> author: ${filterBook.author_name[0]} </h3>
+                        <h4 class="card-text"> publisher: ${filterBook.publisher[0]} </h4>
                         <h6 class="card-text"> First publishing year: ${filterBook.first_publish_year} </h6>
                 </div>
             </div>
@@ -85,3 +82,16 @@ const displayBooks = (books, numbers) => {
 
 
 
+
+
+
+
+/* <div class="card h-100">
+    <div class="card-body border border-success">
+        <img class="card-img-top mb-5" src="https://covers.openlibrary.org/b/id/${filterBook.cover_i}-M.jpg">
+        <h1 class="card-title text-center"> ${filterBook.title}</h1>
+        <h3 class="card-text"> author: ${filterBook.author_name[0]} </h3>
+                        <h4 class="card-text"> publisher: ${filterBook.publisher[0]} </h4>
+                        <h6 class="card-text"> First publishing year: ${filterBook.first_publish_year} </h6>
+                           </div>
+            </div>*/
